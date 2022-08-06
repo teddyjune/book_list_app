f0b09ae51e4cdebff988df39704ed5d21cadab27
yeonjoo.lee <teddyjune@naver.com>
2022-08-06 20:36:58 +0900

    add: flutterfire UI(구글)

--- lib/root/root_screen.dart
+++ lib/root/root_screen.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:untitled/book_list/book_list_screen.dart';
-import 'package:untitled/login/login_screen.dart';
+
 
 class RootScreen extends StatelessWidget {
   const RootScreen({Key? key}) : super(key: key);
@@ -14,8 +14,13 @@ class RootScreen extends StatelessWidget {
       builder: (context, snapshot) {
         User? user = snapshot.data;
         if (user == null) {
-          return const SignInScreen(
-              providerConfigs: [EmailProviderConfiguration()]);
+          return const SignInScreen(providerConfigs: [
+            GoogleProviderConfiguration(
+              clientId:
+                  '245549996846-va5g89d9u0u9aptuftb6it7d18l36tur.apps.googleusercontent.com',
+            ),
+            EmailProviderConfiguration(),
+          ]);
         }
         return BookListScreen();
       },
